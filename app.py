6b463155561281a73c7f796c437440b2a7a17d78
minimalex <73164565+minimalex@users.noreply.github.com>
2024-06-18 13:52:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from langchain.agents import AgentExecutor, create_openai_tools_agent
 from langchain.prompts.chat import ChatPromptTemplate
 from langchain_core.tools import tool
 from langchain_core.prompts import MessagesPlaceholder
-from langchain_pinecone import PineconeVectorStore
 import pinecone
 
 # Initialize Pinecone
